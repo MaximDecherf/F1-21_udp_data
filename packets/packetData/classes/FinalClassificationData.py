6eacc7909f793fea9e32cb41405047b0cb4c9aff
MaximDecherf <52461982+MaximDecherf@users.noreply.github.com>
2021-08-06 18:19:13 +0200

    final classification data number decoder

--- packets/packetData/classes/FinalClassificationData.py
+++ packets/packetData/classes/FinalClassificationData.py
@@ -4,7 +4,9 @@ class FinalClassificationData:
 
     BYTES_SPLITS = {'position' : [True, int, 1], 'num_laps' : [True, int, 2], 'grid_position' : [True, int, 3], 'points' : [True, int, 4], 'num_pit_stops' : [True, int, 5], 'result_status' : [True, int, 6], 'best_lap_time_in_MS' : [True, int, 10], 'total_race_time' : [True, 'double', 18], 'penalties_time' : [True, int, 19], 'num_penalties' : [True, int, 20],  'num_tyre_stints' : [True, int, 21], 'tyre_stints_actual' : [False, 1, int, 29], 'tyre_stints_visual' : [False, 1, int, 37]}
 
-    def __init__(self, data):
+    NUMBER_DECODER = {'result_status' : ['invalid', 'inactive', 'active', 'finished', 'dnf', 'disqualified', 'not classified', 'retired']}
+
+    def __init__(self, data, decode_numbers=True):
         end_prev = 0
         for key, value in self.BYTES_SPLITS.items():
             if value[0]:
@@ -25,6 +27,17 @@ class FinalClassificationData:
                     end_prev = end_prev+value[1]
                 setattr(self, key, data_list)
                 end_prev = value[3]
+        if decode_numbers:
+            self.decode_numbers() 
+    
+    
+    def decode_numbers(self):
+        for key, decoder in self.NUMBER_DECODER.items():
+            int_value = getattr(self, key)
+            if int_value < 0:
+                setattr(self, key, 'invalid')
+            else:
+                setattr(self, key, decoder[int_value])
     
     def __repr__(self):
         return str(self.__dict__)
