f580b37c287d0a8e06f262b76c58ef023cdfff1b
MaximDecherf <52461982+MaximDecherf@users.noreply.github.com>
2021-08-06 18:21:11 +0200

    lobby info data number decoder

--- packets/packetData/classes/LobbyInfoData.py
+++ packets/packetData/classes/LobbyInfoData.py
@@ -2,13 +2,26 @@ import re
 
 class LobbyInfoData:
 
-    def __init__(self, data):
+    NUMBER_DECODER = {'ai_controlled' : ['human', 'ai'], 'ready_status' : ['not ready', 'ready', 'spectating']}
+
+    def __init__(self, data, decode_numbers=True):
         self.ai_controlled = int.from_bytes(data[0:1], byteorder='little')
         self.team_id = int.from_bytes(data[1:2], byteorder='little')
         self.nationality = int.from_bytes(data[2:3], byteorder='little')
         self.name = re.sub(r'\x00', '', data[3:52].decode("utf-8")) #cleans up name 
         self.ready_status = int.from_bytes(data[52:53], byteorder='little')
+        if decode_numbers:
+            self.decode_numbers() 
     
+    
+    def decode_numbers(self):
+        for key, decoder in self.NUMBER_DECODER.items():
+            int_value = getattr(self, key)
+            if int_value < 0:
+                setattr(self, key, 'invalid')
+            else:
+                setattr(self, key, decoder[int_value])
+
     def __repr__(self):
         return str(self.__dict__)
     
