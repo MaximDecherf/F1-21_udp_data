2af5641a9d358c3e9ac34cbf62338b37cebf3d1c
MaximDecherf <52461982+MaximDecherf@users.noreply.github.com>
2021-08-06 18:37:45 +0200

    refractoring of carMotionData

--- packets/packetData/classes/CarMotionData.py
+++ packets/packetData/classes/CarMotionData.py
@@ -1,47 +1,34 @@
 import struct
 
 class CarMotionData():
-    BYTES_SPLITS_INT = {'world_forward_dir_x' : 26, 'world_forward_dir_y' : 28, 'world_forward_dir_z' : 30, 'world_right_dir_x' : 32 , 'world_right_dir_y' : 34, 'world_right_dir_z' : 36}
-    #TODO : normaliszed data and refactor code
+    #TODO : normaliszed data 
 
-    BYTES_SPLITS_FLOAT = {'world_position_x' : 4, 'world_position_y' : 8, 'world_position_z' : 12, 'world_velocity_x' : 16 , 'world_velocity_y' : 20, 'world_velocity_z' : 24, 'g_force_lateral' : 40, 'g_force_longitudinal' : 44, 'g_force_vertical' : 48, 'yaw' : 52, 'pitch' : 56, 'roll' : 60}
+    BYTES_SPLITS = {'world_position_x' : [True, float ,4], 'world_position_y' : [True, float, 8], 'world_position_z' : [True, float, 12], 'world_velocity_x' : [True, float, 16] , 'world_velocity_y' : [True, float, 20], 'world_velocity_z' : [True, float, 24], 
+    'world_forward_dir_x' : [True, int, 26], 'world_forward_dir_y' : [True, int, 28], 'world_forward_dir_z' : [True, int, 30], 'world_right_dir_x' : [True, int, 32] , 'world_right_dir_y' : [True, int, 34], 'world_right_dir_z' : [True, int, 36], 'g_force_lateral' : [True, float, 40], 'g_force_longitudinal' : [True, float, 44], 'g_force_vertical' : [True, float, 48], 'yaw' : [True, float, 52], 'pitch' : [True, float, 56], 'roll' : [True, float, 60]}
 
     SIZE = 60
 
     def __init__(self, data):
-        end_prev = 24 # int values start from byte 24 until byte 36
         if len(data) == self.SIZE:
-            for key, value in self.BYTES_SPLITS_INT.items():
-                setattr(self, key, int.from_bytes(data[end_prev:value], byteorder='little'))
-                end_prev = value
+            end_prev = 0
+            for key, value in self.BYTES_SPLITS.items():
+                if value[0]:
+                    if value[1] == int:
+                            setattr(self, key, int.from_bytes(data[end_prev:value[2]], byteorder='little'))
+                            end_prev = value[2]
+                    elif value[1] == float:
+                            setattr(self, key, struct.unpack('<f', data[end_prev:value[2]]))
+                            end_prev = value[2]
 
-        
-            end_prev = 0 # float values start from byte 0 until byte 24 and from byte 36 until byte 60
-            for key, value in self.BYTES_SPLITS_FLOAT.items():
-                setattr(self, key, struct.unpack('<f', data[end_prev:value]))
-                end_prev = value
-                if end_prev == 24: #to jump the gap of bytes, is bad coding probally but don't know how to fix it 
-                    end_prev = 36
-
-            self.data_lenhgt = len(data)
+                self.data_lenhgt = len(data)
 
         else:
             raise ValueError('Data must be size of {0} bytes'.format(self.SIZE)) 
 
-    def __str__(self):
-        return str(
-            self.get_all_data()
-        )
-
-    def get_all_data(self):
-        data = dict()
-        for key , value in self.BYTES_SPLITS_FLOAT.items():
-            data.update({key : getattr(self, key)})
-        for key, value in self.BYTES_SPLITS_INT.items():
-            data.update({key : getattr(self, key)})
-        return data
-                    
-
     def __repr__(self):
-        return str(self.get_all_data())
+        return str(self.__dict__)
+    
+    def __str__(self):
+        full_dict = self.__dict__
+        return str(self.__class__) + " : " + str(full_dict)
 
